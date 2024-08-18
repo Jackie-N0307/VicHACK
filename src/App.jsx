1dda774f17c3f4e70c5d6afe9c28014a75a18596
FinnyEwe <141378058+FinnyEwe@users.noreply.github.com>
2024-08-18 12:30:02 +1000

    half of frontend

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+import './App.css'
+import PlayerScreen from './player.jsx'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import FrontScreen from './frontscreen.jsx';
+import { GlobalProvider } from './GlobalContext';
+
+
+
+function App() {
+
+  return (
+    
+    <>
+    <GlobalProvider>
+      <Router>
+        <Routes>
+          <Route path="/playerscreen" element={<PlayerScreen />} />
+          <Route path="/" element={<FrontScreen />} />
+        </Routes>
+      </Router>
+    </GlobalProvider>
+    </>
+  )
+}
+
+export default App
